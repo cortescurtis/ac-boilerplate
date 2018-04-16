873f19913b0cbdf5ef88513bd335c95720ed6692
Curtis McLaws <cortescurtis@gmail.com>
2018-04-16 17:05:22 -0600

    3a

--- src/products/store/effects/pizzas.effect.ts
+++ src/products/store/effects/pizzas.effect.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Effect, Actions } from '@ngrx/effects';
+import { map, catchError, exhaustMap } from 'rxjs/operators';
 
 import { of } from 'rxjs/observable/of';
 
@@ -8,8 +10,23 @@ import * as fromServices from '../../services';
 
 @Injectable()
 export class PizzasEffects {
+  
+  @Effect()
+  private loadPizzas$ = this.actions$.ofType(pizzaActions.LOAD_PIZZAS)
+    .pipe(
+      exhaustMap(() => {
+        return this.pizzaService
+        .getPizzas()
+        .pipe(
+          map(pizzas => new pizzaActions.LoadPizzasSuccess(pizzas)),
+          catchError(error => of(new pizzaActions.LoadPizzasFail(error)))
+        )
+      })
+    );
+  
   constructor(
     private router: Router,
-    private pizzaService: fromServices.PizzasService
+    private pizzaService: fromServices.PizzasService,
+    private actions$: Actions
   ) {}
 }
