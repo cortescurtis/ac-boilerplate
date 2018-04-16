32eb6b236b1a53d5a69910269978157207249d89
Curtis McLaws <cortescurtis@gmail.com>
2018-04-16 16:16:37 -0600

    2c

--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -1,8 +1,30 @@
 import * as fromPizzas from '../actions/pizzas.action';
 import { Pizza } from '../../models/pizza.model';
 
-export interface PizzaState {}
+export interface PizzaState {
+    loaded: boolean,
+    loading: boolean,
+    pizzas: Pizza[]
+}
 
-const initialState: PizzaState = {};
+const initialState: PizzaState = {
+    loaded: false,
+    loading: false,
+    pizzas: []
+};
 
-export function reducer() {}
+export function reducer(state = initialState, action: fromPizzas.PizzasAction): PizzaState {
+    switch(action.type) {
+        case fromPizzas.LOAD_PIZZAS: {
+            const pizzas = [...state.pizzas];
+            const loaded = false;
+            const loading = false;
+            return {
+                pizzas,
+                loaded,
+                loading
+            }
+        }
+    }
+    return state;
+}
