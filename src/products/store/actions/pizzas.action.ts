8cbb08acd18c243d0039301526d911421340ed98
Curtis McLaws <cortescurtis@gmail.com>
2018-04-16 16:05:51 -0600

    2a

--- src/products/store/actions/pizzas.action.ts
+++ src/products/store/actions/pizzas.action.ts
@@ -2,7 +2,13 @@ import { Action } from '@ngrx/store';
 
 import { Pizza } from '../../models/pizza.model';
 
+export const LOAD_PIZZAS = '[Feature] Load Pizzas';
 export const MY_ACTION = '[Feature] My Action';
+
+export class LoadPizzas implements Action {
+  readonly type = LOAD_PIZZAS;
+  constructor() {}
+}
 
 export class MyAction implements Action {
   readonly type = MY_ACTION;
@@ -10,4 +16,4 @@ export class MyAction implements Action {
 }
 
 // action types
-export type PizzasAction = MyAction;
+export type PizzasAction = MyAction | LoadPizzas;
